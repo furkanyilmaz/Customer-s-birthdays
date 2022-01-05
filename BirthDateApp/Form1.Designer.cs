b1539f822133ff9117872145aed8172601a9740c
Furkan <furkanyilmaz-@hotmail.com>
2022-01-05 19:41:55 +0300

    will be held

---
+++ BirthDateApp/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace BirthDateApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.musteriAdEkle = new System.Windows.Forms.TextBox();
+            this.musteriSoyadEkle = new System.Windows.Forms.TextBox();
+            this.musteriMeslekEkle = new System.Windows.Forms.TextBox();
+            this.musteriAd = new System.Windows.Forms.Label();
+            this.musteriSoyad = new System.Windows.Forms.Label();
+            this.musteriMeslek = new System.Windows.Forms.Label();
+            this.musteriAdres = new System.Windows.Forms.Label();
+            this.musteriTelEkle = new System.Windows.Forms.TextBox();
+            this.musteriAdresEkle = new System.Windows.Forms.RichTextBox();
+            this.musteriKaydet = new System.Windows.Forms.Button();
+            this.dogumtarihi = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.sil = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.guncelle = new System.Windows.Forms.Button();
+            this.idEkle = new System.Windows.Forms.TextBox();
+            this.id = new System.Windows.Forms.Label();
+            this.musteriTel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listeleButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // musteriAdEkle
+            // 
+            this.musteriAdEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriAdEkle.Location = new System.Drawing.Point(131, 137);
+            this.musteriAdEkle.Name = "musteriAdEkle";
+            this.musteriAdEkle.Size = new System.Drawing.Size(248, 26);
+            this.musteriAdEkle.TabIndex = 0;
+            // 
+            // musteriSoyadEkle
+            // 
+            this.musteriSoyadEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriSoyadEkle.Location = new System.Drawing.Point(131, 179);
+            this.musteriSoyadEkle.Name = "musteriSoyadEkle";
+            this.musteriSoyadEkle.Size = new System.Drawing.Size(248, 26);
+            this.musteriSoyadEkle.TabIndex = 2;
+            // 
+            // musteriMeslekEkle
+            // 
+            this.musteriMeslekEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriMeslekEkle.Location = new System.Drawing.Point(131, 296);
+            this.musteriMeslekEkle.Name = "musteriMeslekEkle";
+            this.musteriMeslekEkle.Size = new System.Drawing.Size(248, 26);
+            this.musteriMeslekEkle.TabIndex = 4;
+            // 
+            // musteriAd
+            // 
+            this.musteriAd.AutoSize = true;
+            this.musteriAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriAd.Location = new System.Drawing.Point(33, 143);
+            this.musteriAd.Name = "musteriAd";
+            this.musteriAd.Size = new System.Drawing.Size(88, 20);
+            this.musteriAd.TabIndex = 6;
+            this.musteriAd.Text = "Müşteri Adı";
+            // 
+            // musteriSoyad
+            // 
+            this.musteriSoyad.AutoSize = true;
+            this.musteriSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriSoyad.Location = new System.Drawing.Point(47, 182);
+            this.musteriSoyad.Name = "musteriSoyad";
+            this.musteriSoyad.Size = new System.Drawing.Size(57, 20);
+            this.musteriSoyad.TabIndex = 7;
+            this.musteriSoyad.Text = "Soyadı";
+            // 
+            // musteriMeslek
+            // 
+            this.musteriMeslek.AutoSize = true;
+            this.musteriMeslek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriMeslek.Location = new System.Drawing.Point(36, 299);
+            this.musteriMeslek.Name = "musteriMeslek";
+            this.musteriMeslek.Size = new System.Drawing.Size(63, 20);
+            this.musteriMeslek.TabIndex = 9;
+            this.musteriMeslek.Text = "Mesleği";
+            // 
+            // musteriAdres
+            // 
+            this.musteriAdres.AutoSize = true;
+            this.musteriAdres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriAdres.Location = new System.Drawing.Point(41, 342);
+            this.musteriAdres.Name = "musteriAdres";
+            this.musteriAdres.Size = new System.Drawing.Size(54, 20);
+            this.musteriAdres.TabIndex = 10;
+            this.musteriAdres.Text = "Adresi";
+            // 
+            // musteriTelEkle
+            // 
+            this.musteriTelEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriTelEkle.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.musteriTelEkle.Location = new System.Drawing.Point(131, 217);
+            this.musteriTelEkle.Name = "musteriTelEkle";
+            this.musteriTelEkle.Size = new System.Drawing.Size(248, 26);
+            this.musteriTelEkle.TabIndex = 11;
+            // 
+            // musteriAdresEkle
+            // 
+            this.musteriAdresEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriAdresEkle.Location = new System.Drawing.Point(131, 339);
+            this.musteriAdresEkle.Name = "musteriAdresEkle";
+            this.musteriAdresEkle.Size = new System.Drawing.Size(248, 52);
+            this.musteriAdresEkle.TabIndex = 12;
+            this.musteriAdresEkle.Text = "";
+            // 
+            // musteriKaydet
+            // 
+            this.musteriKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriKaydet.Location = new System.Drawing.Point(185, 416);
+            this.musteriKaydet.Name = "musteriKaydet";
+            this.musteriKaydet.Size = new System.Drawing.Size(176, 61);
+            this.musteriKaydet.TabIndex = 13;
+            this.musteriKaydet.Text = "Kaydet";
+            this.musteriKaydet.UseVisualStyleBackColor = true;
+            this.musteriKaydet.Click += new System.EventHandler(this.musteriKaydet_Click);
+            // 
+            // dogumtarihi
+            // 
+            this.dogumtarihi.AutoSize = true;
+            this.dogumtarihi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dogumtarihi.Location = new System.Drawing.Point(18, 264);
+            this.dogumtarihi.Name = "dogumtarihi";
+            this.dogumtarihi.Size = new System.Drawing.Size(103, 20);
+            this.dogumtarihi.TabIndex = 14;
+            this.dogumtarihi.Text = "Doğum Tarihi";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(131, 259);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(248, 26);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // sil
+            // 
+            this.sil.Location = new System.Drawing.Point(185, 550);
+            this.sil.Name = "sil";
+            this.sil.Size = new System.Drawing.Size(176, 37);
+            this.sil.TabIndex = 17;
+            this.sil.Text = "Sil";
+            this.sil.UseVisualStyleBackColor = true;
+            this.sil.Click += new System.EventHandler(this.sil_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(411, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(648, 527);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // guncelle
+            // 
+            this.guncelle.Location = new System.Drawing.Point(185, 496);
+            this.guncelle.Name = "guncelle";
+            this.guncelle.Size = new System.Drawing.Size(176, 32);
+            this.guncelle.TabIndex = 20;
+            this.guncelle.Text = "Güncelle";
+            this.guncelle.UseVisualStyleBackColor = true;
+            this.guncelle.Click += new System.EventHandler(this.guncelle_Click);
+            // 
+            // idEkle
+            // 
+            this.idEkle.Enabled = false;
+            this.idEkle.Location = new System.Drawing.Point(131, 97);
+            this.idEkle.Name = "idEkle";
+            this.idEkle.Size = new System.Drawing.Size(45, 20);
+            this.idEkle.TabIndex = 21;
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.id.Location = new System.Drawing.Point(36, 97);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(85, 20);
+            this.id.TabIndex = 22;
+            this.id.Text = "Müşteri No";
+            // 
+            // musteriTel
+            // 
+            this.musteriTel.AutoSize = true;
+            this.musteriTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.musteriTel.Location = new System.Drawing.Point(47, 220);
+            this.musteriTel.Name = "musteriTel";
+            this.musteriTel.Size = new System.Drawing.Size(62, 20);
+            this.musteriTel.TabIndex = 23;
+            this.musteriTel.Text = "Telefon";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1078, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 524);
+            this.listBox1.TabIndex = 24;
+            // 
+            // listeleButton
+            // 
+            this.listeleButton.Location = new System.Drawing.Point(1161, 12);
+            this.listeleButton.Name = "listeleButton";
+            this.listeleButton.Size = new System.Drawing.Size(121, 41);
+            this.listeleButton.TabIndex = 25;
+            this.listeleButton.Text = "bugün doğum günü olanları listele";
+            this.listeleButton.UseVisualStyleBackColor = true;
+            this.listeleButton.Click += new System.EventHandler(this.listeleButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1375, 599);
+            this.Controls.Add(this.listeleButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.musteriTel);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.idEkle);
+            this.Controls.Add(this.guncelle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.sil);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dogumtarihi);
+            this.Controls.Add(this.musteriKaydet);
+            this.Controls.Add(this.musteriAdresEkle);
+            this.Controls.Add(this.musteriTelEkle);
+            this.Controls.Add(this.musteriAdres);
+            this.Controls.Add(this.musteriMeslek);
+            this.Controls.Add(this.musteriSoyad);
+            this.Controls.Add(this.musteriAd);
+            this.Controls.Add(this.musteriMeslekEkle);
+            this.Controls.Add(this.musteriSoyadEkle);
+            this.Controls.Add(this.musteriAdEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox musteriAdEkle;
+        private System.Windows.Forms.TextBox musteriSoyadEkle;
+        private System.Windows.Forms.TextBox musteriMeslekEkle;
+        private System.Windows.Forms.Label musteriAd;
+        private System.Windows.Forms.Label musteriSoyad;
+        private System.Windows.Forms.Label musteriMeslek;
+        private System.Windows.Forms.Label musteriAdres;
+        private System.Windows.Forms.TextBox musteriTelEkle;
+        private System.Windows.Forms.RichTextBox musteriAdresEkle;
+        private System.Windows.Forms.Button musteriKaydet;
+        private System.Windows.Forms.Label dogumtarihi;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button sil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button guncelle;
+        private System.Windows.Forms.TextBox idEkle;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label musteriTel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button listeleButton;
+    }
+}
+
---
+++ BirthDateApp/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BirthDateApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void MusteriGetir()
+        {
+            baglanti = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Database= DateOfBirth;Trusted_Connection=true;");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT *FROM [Table]", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MusteriGetir();
+        }
+
+        private void musteriKaydet_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO [Table](ad,soyad,telefon,dogumtarihi,meslek,adres) VALUES (@ad,@soyad,@telefon,@dogumtarihi,@meslek,@adres)";
+            komut = new SqlCommand(sorgu, baglanti);
+
+            komut.Parameters.AddWithValue("@ad", musteriAdEkle.Text);
+            komut.Parameters.AddWithValue("@soyad", musteriSoyadEkle.Text);
+            komut.Parameters.AddWithValue("@telefon", musteriTelEkle.Text);
+            komut.Parameters.AddWithValue("@dogumtarihi", dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@meslek", musteriMeslekEkle.Text);
+            komut.Parameters.AddWithValue("@adres", musteriAdresEkle.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            idEkle.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            musteriAdEkle.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            musteriSoyadEkle.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            musteriTelEkle.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            musteriMeslekEkle.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            musteriAdresEkle.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+        }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM [Table] WHERE id=@id";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@id", Convert.ToInt32(idEkle.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+        }
+
+        private void guncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE [Table] SET ad=@ad,soyad=@soyad,telefon=@telefon,dogumtarihi=@dogumtarihi,meslek=@meslek,adres=@adres WHERE id=@id";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@id", Convert.ToInt32(idEkle.Text));
+            komut.Parameters.AddWithValue("@ad", musteriAdEkle.Text);
+            komut.Parameters.AddWithValue("@soyad", musteriSoyadEkle.Text);
+            komut.Parameters.AddWithValue("@telefon", musteriTelEkle.Text);
+            komut.Parameters.AddWithValue("@dogumtarihi", dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@meslek", musteriMeslekEkle.Text);
+            komut.Parameters.AddWithValue("@adres", musteriAdresEkle.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+
+        }
+
+        private void listeleButton_Click(object sender, EventArgs e)
+        {
+            Listele_LBOX();
+        }
+
+
+
+
+        void Listele_LBOX()
+        {
+            string sorgu = "SELECT ad,soyad,dogumtarihi FROM[Table] WHERE DAY(dogumtarihi) = DAY(GETDATE()) AND MONTH(dogumtarihi) = MONTH(GETDATE())";
+            komut = new SqlCommand(sorgu, baglanti);
+            baglanti.Open();
+            dr = komut.ExecuteReader();
+
+            while (dr.Read())
+            {
+                listBox1.Items.Add(dr["ad"] + "\t" + dr["soyad"]);
+
+            }
+            baglanti.Close();
+
+
+            //listBox1.Items.Add(dataGridView1.Rows[i].Cells["ad"].Value.ToString());
+            //listBox1.Items.Add(dataGridView1.Rows[i].Cells["dogumtarihi"].Value.ToString());
+        }
+    }
+}
